ca2480e05c5ac14c61ab8fe7ac824e638342f362
Robbie R <git@robbie.computer>
2021-11-16 09:16:58 +0000

    Adjust layout, remove logging

--- components/header/index.js
+++ components/header/index.js
@@ -1,10 +1,12 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { GridItem, Heading, Text } from '@chakra-ui/react';
 
 export default function Header() {
   return (
     <header>
-      <Heading>gm portal</Heading>
-      <Text>It is time to say gm</Text>
+      <GridItem rowstart={1} rowEnd={1}>
+        <Heading>gm portal</Heading>
+        <Text>It is time to say gm</Text>
+      </GridItem>
     </header>
   );
 }
